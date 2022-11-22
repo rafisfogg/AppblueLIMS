d1a5dfefa2f08339c1e9d448f4cd08f4fa9b9506
rafisfogg <98602070+rafisfogg@users.noreply.github.com>
2022-11-22 09:27:46 -0300

    Initial Commit

---
+++ AppblueLIMS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppblueLIMS
+//
+//  Created by IFB BIOTIC 11 on 22/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
